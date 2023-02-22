71886e7c2f58a79a62d61efc12e05740696bbbdd
21BTen <98640569+21BTen@users.noreply.github.com>
2023-02-22 18:19:29 +0600

    share button

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,6 +4,7 @@ export interface Product {
   price: number;
   description: string;
   url: string;
+  kaspi_link: string;
 }
 
 export const products = [
@@ -11,22 +12,17 @@ export const products = [
     id: 1,
     name: 'ONYX BOOX Nova Air2',
     price: 185000,
-    description: 'A large phone with one of the best screens',
+    description: 'E-book ONYX BOOX Nova Air2 white,built-in backlight',
     url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h78/h66/66012099215390/onyx-boox-nova-air2-belyi-107673318-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-nova-air2-belyi-107673318/?c=750000000#!/item'
   },
   {
     id: 2,
     name: 'ONYX BOOX NOTE AIR 2 PLUS',
     price: 255000,
-    description: 'A great phone with one of the best cameras',
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
     url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
-  },
-  {
-    id: 3,
-    name: 'Amazon Kindle 10',
-    price: 299,
-    description: '',
-    url: 'https://image.winudf.com/v2/image/bWUud2FsbHBhcGEubmF0dXJlX3NjcmVlbl8xXzE1MzIzNzgxNThfMDE3/screen-1.jpg?fakeurl=1&type=.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
   },
 ];
 
