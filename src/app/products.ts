22722bcde7d09776b04b9111b8443e3a54a2c7cd
21BTen <98640569+21BTen@users.noreply.github.com>
2023-02-22 18:48:03 +0600

    changes with price

--- src/app/products.ts
+++ src/app/products.ts
@@ -24,6 +24,71 @@ export const products = [
     url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
     kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
   },
+
+  {
+    id: 3,
+    name: 'ONYX VASCO DA GAMA',
+    price: 70993,
+    description: 'E-book ONYX VASCO DA GAMA 4 black ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdc/h58/66423830708254/onyx-vasco-da-gama-4-chernyi-107846588-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-vasco-da-gama-4-chernyi-107846588/?c=750000000#!/item'
+  },
+  {
+    id: 4,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 179990,
+    description: 'E-book ONYX BOOX Poke 3 black,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hcd/h67/46191145189406/onyx-boox-poke-3-cernyj-101138483-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-3-chernyi-101138483/?c=750000000#!/item'
+  },
+  {
+    id: 5,
+    name: 'ONYX BOOX Poke 4 Lite',
+    price: 85000,
+    description: 'ONYX BOOX Poke 4 Lite is a compact reader with a 6—inch E Ink Carta screen, with MOON Light 2 backlight and touch control. The model is made in a thin lightweight case, its weight is only 150 grams. The reader is based on a modern high-performance processor and has 2 GB of RAM and 16 GB of internal memory. The built-in Wi-Fi module allows you to access the Internet and download books directly to your device, and Bluetooth allows you to connect headphones, speakers, keyboards and much more.',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he1/h1f/52430623047710/onyx-boox-poke-4-lite-belyj-105776884-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-poke-4-lite-belyi-105776884/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'ONYX BOOX NOTE AIR 2 PLUS',
+    price: 255000,
+    description: 'E-book ONYX BOOX NOTE AIR 2 PLUS green ,built-in backlight',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/h95/51932230451230/onyx-boox-note-air-2-plus-multikolor-105524843-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/onyx-boox-note-air-2-plus-zelenyi-105524843/?c=750000000#!/item'
+  },
 ];
 
 /*
